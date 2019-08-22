58aee0356837f1d6d7e1ad9565df328abb534fed
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-08-23 00:36:56 +0300

    Initial commit;

---
+++ src/main/java/JdbcTemplate.java
@@ -0,0 +1,19 @@
+import mapper.RowMapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface JdbcTemplate {
+
+    <T> List<T> query(String query, RowMapper<T> rowMapper, Object...args);
+
+    <T> T queryForObject (String query, RowMapper<T> rowMapper, Object...args);
+
+    int update (String query, Object... args );
+
+    <T> List<T> query(String query, RowMapper<T> rowMapper, Map<String, ?> params);
+
+    <T> T queryForObject(String query, RowMapper<T> rowMapper, Map<String, ?> params);
+
+    int update(String query, Map<String, ?> params);
+}
