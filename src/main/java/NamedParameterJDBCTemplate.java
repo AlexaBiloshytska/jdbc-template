277e11ff5f919e97be3a56eb785127561b0896c9
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-09-02 21:57:27 +0300

    update with varargs method refactoring;
    tests added;
    
    updateQuery implemented;
    tests added;

--- src/main/java/NamedParameterJDBCTemplate.java
+++ src/main/java/NamedParameterJDBCTemplate.java
@@ -1,37 +1,24 @@
 import com.zaxxer.hikari.HikariDataSource;
 import executor.QueryExecutor;
 import mapper.RowMapper;
-import template.EntityBuilder;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class NamedParameterJDBCTemplate implements JdbcTemplate {
-    private DataSource dataSource;
-    private RowMapper rowMapper;
+    private QueryExecutor queryExecutor;
 
     public NamedParameterJDBCTemplate(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.queryExecutor = new QueryExecutor(dataSource);
     }
 
-    /**
-     * @param query - sql query with placeholders
-     * @param rowMapper  - map row to object
-     * @param args - placeholder parameters
-     * @return List<T> - Objects list which mapped with resultSet;
-     */
     @Override
     public <T> List<T> query(String query, RowMapper<T> rowMapper, Object... args) {
-            QueryExecutor queryExecutor = new QueryExecutor(dataSource);
-            List<Object> params = Arrays.asList(args);
-            List<T> resultSet = queryExecutor.executeQuery(query, params,rowMapper);
-            return resultSet;
+        List<Object> params = Arrays.asList(args);
+        return queryExecutor.executeQuery(query, params, rowMapper);
     }
 
     @Override
@@ -41,7 +28,8 @@ public class NamedParameterJDBCTemplate implements JdbcTemplate {
 
     @Override
     public int update(String query, Object... args) {
-        return 0;
+        List<?> paramsList = new ArrayList<>(Arrays.asList(args));
+        return queryExecutor.executeUpdate(query, paramsList);
     }
 
     @Override
---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,13 @@
+package parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParserTest {
+
+}
